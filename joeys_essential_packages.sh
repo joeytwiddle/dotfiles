3bcfb3dca57120d2e57a2cb38fbfec1da01283ab
Paul Joey Clark <joey@neuralyte.org>
2013-10-09 11:58:57 +0800

    packages i hate to live without

---
+++ joeys_essential_packages.sh
@@ -0,0 +1,5 @@
+packages_editor="vim-gtk exuberant-ctags"
+packages_winman="xfonts-75dpi xfonts-100dpi fluxbox xdotool wmctrl xscreensaver imagemagick"
+packages_ui="geeqie gkrellm"
+
+aptitude install $packages_editor $packages_winman $packages_ui
