988a343d823ba48ab13e723d36f431ae41667cf1
Paul Joey Clark <joey@neuralyte.org>
2013-10-09 12:03:17 +0800

    packages i hate to live without

--- joeys_essential_packages.sh
+++ joeys_essential_packages.sh
@@ -1,5 +1,6 @@
 packages_editor="vim-gtk exuberant-ctags"
 packages_winman="xfonts-75dpi xfonts-100dpi fluxbox xdotool wmctrl xscreensaver imagemagick"
 packages_ui="geeqie gkrellm"
+packages_remote="openssh-server screen xtightvncviewer x11vnc tightvncserver"
 
-aptitude install $packages_editor $packages_winman $packages_ui
+sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote
