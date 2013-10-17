7b79b2eca200cec10b763e5c220ce4bd26e5892f
Paul Joey Clark <joey@neuralyte.org>
2013-10-17 10:35:31 +0800

    more favourites

--- joeys_essential_packages.sh
+++ joeys_essential_packages.sh
@@ -1,6 +1,10 @@
 packages_editor="vim-gtk exuberant-ctags"
 packages_winman="xfonts-75dpi xfonts-100dpi fluxbox xdotool wmctrl xscreensaver imagemagick"
 packages_ui="geeqie gkrellm"
+#zsh
 packages_remote="openssh-server screen xtightvncviewer x11vnc tightvncserver"
 
-sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote
+#packages_debugging="iotop nmap"
+#packages_debugging="$packages_debugging wireshark"
+
+sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote $packages_debugging
