9b9285c66321059b4885ed7fb724ebbb4a960716
Paul Joey Clark <joey@neuralyte.org>
2013-10-28 14:38:18 +0800

    optional extras

--- joeys_essential_packages.sh
+++ joeys_essential_packages.sh
@@ -6,5 +6,7 @@ packages_remote="openssh-server screen xtightvncviewer x11vnc tightvncserver"
 
 #packages_debugging="iotop nmap"
 #packages_debugging="$packages_debugging wireshark"
+packages_useful="dict-wn"
+packages_winman="xfstt"   # To get LucidaConsole in GVim!
 
-sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote $packages_debugging
+sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote $packages_debugging $packages_useful
