8ab423e4f1d0f82e01c99a2ca3cb00b62eb56b58
Paul Joey Clark <joey@neuralyte.org>
2013-10-30 11:09:47 +0800

    added video player (but no music player yet), added hugs and atop

--- joeys_essential_packages.sh
+++ joeys_essential_packages.sh
@@ -1,12 +1,11 @@
 packages_editor="vim-gtk exuberant-ctags"
 packages_winman="xfonts-75dpi xfonts-100dpi fluxbox xdotool wmctrl xscreensaver imagemagick"
 packages_ui="geeqie gkrellm"
-#zsh
 packages_remote="openssh-server screen xtightvncviewer x11vnc tightvncserver"
 
-#packages_debugging="iotop nmap"
-#packages_debugging="$packages_debugging wireshark"
-packages_useful="dict-wn"
-packages_winman="xfstt"   # To get LucidaConsole in GVim!
+#packages_winman="xfstt"   # To get LucidaConsole in GVim!  (See ~/FONTS folder)
+#packages_debugging="iotop atop nmap wireshark"
+#packages_desktop_extended="mplayer gimp inkscape libreoffice dict-wn zsh"
+#packages_yummy="hugs"   # ghc is also nice, but 290meg!
 
-sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote $packages_debugging $packages_useful
+sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote $packages_debugging $packages_desktop_extended $packages_yummy
