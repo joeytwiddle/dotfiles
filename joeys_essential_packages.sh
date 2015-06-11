1814931bb8e4cc37ee37ef3e4f2c106ae2898ef9
Paul Joey Clark <joey@neuralyte.org>
2015-06-11 05:13:56 +0000

    added xscreensaver extras, lmsensors and lftp

--- joeys_essential_packages.sh
+++ joeys_essential_packages.sh
@@ -1,16 +1,16 @@
 packages_editor="vim-gtk exuberant-ctags"
-packages_winman="xfonts-75dpi xfonts-100dpi fluxbox xdotool wmctrl xscreensaver imagemagick"
+packages_winman="xfonts-75dpi xfonts-100dpi fluxbox xscreensaver xscreensaver-data-extra imagemagick"
 packages_ui="geeqie gkrellm"
 packages_remote="openssh-server screen xtightvncviewer x11vnc tightvncserver tmux"
 
 #packages_winman="xfstt"   # To get LucidaConsole in GVim!  (See ~/FONTS folder)
-packages_debugging="iotop atop nmap wireshark mesa-utils"
+packages_debugging="iotop atop nmap wireshark mesa-utils lmsensors"
 packages_desktop_extended="mplayer gimp inkscape dict-wn zsh wmctrl xdotool"
 # libreoffice compizconfig-settings-manager 
 packages_yummy="hugs"   # ghc is also nice, but 290meg!
 
 packages_system="cpufrequtils"
 
-packages_development="git-core sshfs encfs unison" # 'git-core' was transitioning to 'git'
+packages_development="git-core ccache sshfs encfs unison lftp" # 'git-core' is transitioning to 'git'
 
 sudo aptitude install $packages_editor $packages_winman $packages_ui $packages_remote $packages_debugging $packages_desktop_extended $packages_yummy $packages_system $packages_development
