bc6eb83486e797dd3139365e3fb3ac86c129abb2
Paul Joey Clark <joeytwiddle@gmail.com>
2019-05-15 14:13:47 +0800

    Add script to help update plugins

---
+++ .vim-addon-manager/update-all-plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TODO What if there is a failure?  Abort and start again once resolved?  Or continue and report failures at the end?
+set -e
+
+for dir in */
+do
+  cd "$dir"
+  if [ -d .git ]
+  then
+    echo "> $dir"
+    git pull
+    echo
+  fi
+  cd ..
+done
