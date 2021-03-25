73ffedb59e0b76eb5f61a3590633c068dd2db24b
Paul Joey Clark <joeytwiddle@gmail.com>
2021-03-26 00:58:06 +0800

    Run various apps after starting KDE

--- .config/joeys-kde-autostart-script.sh
+++ .config/joeys-kde-autostart-script.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-# Load run_if_window_matches and other scripts for sxhkd
+# Sets up PATH
+# Also loads run_if_window_matches and other scripts used by sxhkd
 . "$HOME"/j/startj-simple
+
 sxhkd > /tmp/sxhkd.$USER.out 2>&1 &
 
+# Disable shadow on KDE/Plasma panel
+(
+    sleep 15
+    for WID in `xwininfo -root -tree | sed '/"Plasma": ("plasmashell" "plasmashell")/!d; s/^  *\([^ ]*\) .*/\1/g'`
+    do xprop -id $WID -remove _KDE_NET_WM_SHADOW
+    done
+) &
+
+# Applications I always want to have running
+
+# This is one of the apps that KDE restored automatically, so we don't need to start it here
+# Same for Firefox, all the terminals, etc.
 #gkrellm &
+
+# But for some reason these apps are not automatically restored, so we do need to start them explicitly
+#
+# Note: You may need to add Special Application Settings, so that these start on the desired desktop
+(
+    sleep 30
+
+    #google-chrome &
+    #sleep 60
+
+    code &
+    sleep 20
+) &
+
