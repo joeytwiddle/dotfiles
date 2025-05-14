5e7532b705db00a4a971f5930176bc7dbe612cfd
Paul Joey Clark <joeytwiddle@gmail.com>
2025-05-14 13:38:32 +0100

    Update get_extensions_list.sh and add install_extensions_from_list.sh

--- .config/Code/get_extensions_list.sh
+++ .config/Code/get_extensions_list.sh
@@ -1,1 +1,10 @@
-code --list-extensions > ~/.config/Code/EXTENSIONS.list
+#!/usr/bin/env bash
+set -e
+
+echo "This script will list both enabled AND disabled extensions"
+echo
+echo "So please uninstall any extensions you are not using"
+echo
+sleep 2
+
+code --list-extensions > ~/.config/Code/EXTENSIONS.list.LATEST
--- .config/Code/install_extensions_from_list.sh
+++ .config/Code/install_extensions_from_list.sh
@@ -1,2 +1,10 @@
-cat ~/.config/Code/EXTENSIONS.list | xargs -L 1 echo code --install-extension
+#!/usr/bin/env bash
+set -e
+
+# Installs extensions which are not commented out
+# Removes extensions which are prefixed by #-
+# Leaves extensions which are prefixed by # as they are (they will not be installed or uninstalled, so they are optional)
+
+cat ~/.config/Code/EXTENSIONS.list | grep -v '^#' | grep -v '^$' | xargs -L 1 echo code --install-extension
+cat ~/.config/Code/EXTENSIONS.list | grep '^#-' | sed 's/^#-//' | xargs -L 1 echo code --uninstall-extension
 echo "# To install extensions, rerun with |sh"
